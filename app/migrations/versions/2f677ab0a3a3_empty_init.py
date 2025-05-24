3c875283a808dd50c1c5168995a59bcc8c4c2857
Ernest Kamau <1.kamauernest@gmail.com>
2025-05-24 09:43:02 +0300

    finished migrations

---
+++ app/migrations/versions/2f677ab0a3a3_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2f677ab0a3a3
+Revises: 
+Create Date: 2025-05-24 09:14:34.059333
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f677ab0a3a3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
